be592ca349115a54289c35467c0844f960d157c3
monmichael32 <monmichael32@gmail.com>
2020-07-28 17:26:22 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,39 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Environment = "production"
+  }
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9+sRnqO1shgNfMdnWy/usEjE/Y794EnAQXbX2NJNWEw/F5YlWYM/LqjIIUvkNb6f9UyD4uUV8zu8Z3ss/Y3iugCN3Jp1PycUE2wg8Mco6rNXaJXjJS982/TsdP8JZ7M6Pj327aiG1eqjZeCmc89/eBHmWkxjbQkOOpanCnO13r5ohm8cumkA/J+WgdGdLmf2W2CjjdoAKw2HvuuzI1ebr4w6QF2mmGukrNrlXX4ENOLGV+0RZ1nnEgT0kjAqcDDQYnURYrBNGLLpPzjO6kW8o+XxTPwEr9wPePD1DRXGoY8j5cnpVwYbMupJ6N2HNVyZaIzJ606bPph3enQy1+dxT monicamichael@Monicas-Air"
+}
 resource "aws_instance" "example" {
   ami           = "ami-0a7f1556c36aaf776"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web_rules.id,aws_security_group.ssh_rules.id]
+  key_name = "deployer-key"
   }
 
 resource "aws_instance" "LamoMama" {
   ami           = "ami-0a7f1556c36aaf776"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ssh_rules.id,aws_security_group.web_rules.id]
+key_name = "deployer-key"
 }
 
 resource "aws_security_group" "web_rules" {
   name = "websg"
+  egress {
+    to_port=0
+    protocol=-1
+    from_port=0
+  }
   ingress {
     from_port   = 80
     to_port     = 80
@@ -44,5 +62,12 @@ resource "aws_elb" "elb-YoMama" {
     lb_port            =80
     lb_protocol        ="http"
   }
-instances           =["${aws_instance.example.id}","${aws_instance.LamoMama.id}"]
+
+instances  =["${aws_instance.example.id}","${aws_instance.LamoMama.id}"]
 }
+data "aws_internet_gateway" "default" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = ["vpc-c3b45fbe"]
+  }
+
