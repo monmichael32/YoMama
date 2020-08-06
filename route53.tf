7062ce85b30642af4619b6996cfe58751c26544a
monmichael32 <monmichael32@gmail.com>
2020-08-06 15:08:00 -0600

    considerthesource

--- route53.tf
+++ route53.tf
@@ -1,17 +1,17 @@
 resource "aws_route53_zone" "main" {
-  name = "cardealers.services"
+  name = "considerthesource.io"
 }
 
 resource "aws_route53_record" "YoMama-ns" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "YoMama-ns.cardealers.services"
+  name    = "YoMama-ns.considerthesource.io"
   type    = "NS"
   ttl     = "300"
   records = aws_route53_zone.main.name_servers
 }
 
 resource "aws_route53_record" "YoMama" {
-  name = "YoMama.cardealers.services"
+  name = "YoMama.considerthesource.io"
   zone_id = aws_route53_zone.main.zone_id
   type    = "CNAME"
   ttl     = "60"
